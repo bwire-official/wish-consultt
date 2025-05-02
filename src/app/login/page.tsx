b01a199882cea67608bab0a6894c3a2891632622
Sharon-uzu <victorysharon0@gmail.com>
2025-05-02 21:13:18 +0100

    correction

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,8 @@
 "use client"; 
 import Image from "next/image";
 import Link from "next/link";
-import React from 'react';
-import image from '../Assets/istockphoto-2147587863-612x612.jpg'
+import React from "react";
+import image from "../Assets/istockphoto-2147587863-612x612.jpg"
 import { FaArrowLeft } from "react-icons/fa";
 
 const Login = () => {
@@ -17,7 +17,7 @@ const Login = () => {
       </div>
       <div className="register-form">
 
-      <h4><Link href='/'><FaArrowLeft className="b-i"/></Link></h4>
+      <h4><Link href="/"><FaArrowLeft className="b-i"/></Link></h4>
 
         <h2>Welcome Back</h2>
         <form>
